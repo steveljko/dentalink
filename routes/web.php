1e77e4b553408903181eb75fe7d8c1f00c7696e0
steveljko <steveljko@proton.me>
2025-06-08 18:17:40 +0200

    refactor routes to include authentication middleware

--- routes/web.php
+++ routes/web.php
@@ -8,18 +8,20 @@ use App\Http\Controllers\PatientAttachmentController;
 use App\Http\Controllers\PatientController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/dashboard', DashboardController::class)->name('dashboard');
-
 Route::get('/login', LoginController::class)->name('login');
 Route::post('/login', [LoginController::class, 'handle'])->name('login.handle');
-Route::delete('/logout', LogoutController::class)->name('logout');
 
-Route::resource('patient', PatientController::class);
+Route::group(['middleware' => 'auth'], function () {
+    Route::get('/dashboard', DashboardController::class)->name('dashboard');
+    Route::delete('/logout', LogoutController::class)->name('logout');
 
-Route::get('/patient/{patient}/appointment', [AppointmentController::class, 'create'])->name('patient.create.appointment');
-Route::post('/patient/{patient}/appointment', [AppointmentController::class, 'store'])->name('patient.store.appointment');
+    Route::resource('patient', PatientController::class);
 
-Route::get('/patient/{patient}/attachment', [PatientAttachmentController::class, 'create'])->name('patient.upload');
-Route::post('/patient/{patient}/attachment', [PatientAttachmentController::class, 'store'])->name('patient.attach');
+    Route::get('/patient/{patient}/appointment', [AppointmentController::class, 'create'])->name('patient.create.appointment');
+    Route::post('/patient/{patient}/appointment', [AppointmentController::class, 'store'])->name('patient.store.appointment');
 
-Route::get('/appointment/{appointment}', [AppointmentController::class, 'show'])->name('appointment.show');
+    Route::get('/patient/{patient}/attachment', [PatientAttachmentController::class, 'create'])->name('patient.upload');
+    Route::post('/patient/{patient}/attachment', [PatientAttachmentController::class, 'store'])->name('patient.attach');
+
+    Route::get('/appointment/{appointment}', [AppointmentController::class, 'show'])->name('appointment.show');
+});
